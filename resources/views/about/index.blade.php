25613f3e7359851e36019d95c58106b674d91562
Muhammad Abdurrahman Hafizh <abdurrahman.hafizh02@gmail.com>
2023-07-15 05:03:48 +0700

    first commit

---
+++ resources/views/about/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row bg-success rounded-top shadow-lg">
+        <div class="col-12 text-center">
+            <h2 class="text-warning">About</h2>
+        </div>
+    </div>
+<div class="row bg-warning rounded-bottom mt-3 shadow-lg">
+    <div class="col-12">
+        <div class="content text-success py-3">
+            Nama    <div class="titik-dua-satu">:</div> <b> Muhammad Abdurrahman Hafizh </b> <br>
+            Nim     <div class="titik-dua-dua">:</div> <b> 0702201033 </b> <br>
+            Kelas       <div class="titik-dua-tiga">:</div> <b> SI-2 </b> <br>
+            Mata Kuliah <div class="titik-dua-empat">:</div> <b> Pemrograman Web Lanjutan </b> <br>
+            Dosen <div class="titik-dua-lima">:</div> <b> Suendri, M.Kom </b> <br><br>
+            
+            <b> Alasan memilih membuat Sisfo ini : </b><br><br>
+            
+            Karena saya bekerja di pondok pesantren Amrullah Akbar Rajasayah sebagai Operator Pesantren dan juga untuk meenuhi tugas UAS Mata Kuliah Pemrograman Web lanjutan dengan menggunakan konsep MVC dan menggunakan Framework Laravel dan juga Framework Bootstrap maka dari itu saya membuat sisfo untuk penginputan data guru yang ada di lingkungan pesantren yang dimana untuk pencatatan guru pesantren Amrullah Akbar Rajasyah masih menggunakan sistem lama yaitu dengan cara di catat menggunakan kertas yang dimana pencatatan tersebut belum efesien dan efektif untuk pesantren karena sering terjadi penumpukan data dan kehilangan data
+            </div>
+    </div>
+</div>
+</div>
+@endsection
